618705d646b3cd89f1c3004ae3dc8fe5512f7504
Vidhanvyrs <vidhanvyrs@gmail.com>
2026-01-13 01:11:46 +0530

    deleting test files

--- test2.py
+++
@@ -1,3 +0,0 @@
-# this is a test comment
-print("Hello, World!") # another comment change lol
-# this is a test2 comment
--- yetanothertest.js
+++
@@ -1,8 +0,0 @@
-function search() {
-    // yet another test file
-    console.log("searching");
-
-    console.log("searching");
-    // this is again a test
-
-}
